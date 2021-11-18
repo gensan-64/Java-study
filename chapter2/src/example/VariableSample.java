61865807e2838d3cde60da75335cc4b5011b33ee
gensan-64 <genyuu6240@gmail.com>
2021-11-18 20:15:10 +0900

    変数の理解

---
+++ chapter2/src/example/VariableSample.java
@@ -0,0 +1,11 @@
+package example;
+
+public class VariableSample {
+	public static void main(String[] args) {
+	    int num = 10;
+	    System.out.println(num);
+	    num = 20;
+	    System.out.println(num);
+	}
+
+}
