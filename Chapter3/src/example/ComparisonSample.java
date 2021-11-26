fdbb3ed7addae919a9e74b533dd22902d93c9aa3
gensan-64 <genyuu6240@gmail.com>
2021-11-26 22:29:49 +0900

    関係演算子での演算

---
+++ Chapter3/src/example/ComparisonSample.java
@@ -0,0 +1,15 @@
+package example;
+
+public class ComparisonSample {
+	
+	public static void main(String[] args) {
+		int number = 10;
+		System.out.println(number == 10);
+		System.out.println(number != 10);
+		System.out.println(number > 10);
+		System.out.println(number <10);
+		System.out.println(number >= 10);
+		System.out.println(number <= 10);
+	}
+
+}
