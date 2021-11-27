99828d108eea55e8cb3339747d62df57019c6dbd
gensan-64 <genyuu6240@gmail.com>
2021-11-27 12:44:34 +0900

    if文での条件分岐

---
+++ Chapter3/src/example/IfSample.java
@@ -0,0 +1,18 @@
+package example;
+
+public class IfSample {
+
+	public static void main(String[] args) {
+		String name = "太郎";
+		int age = 24;
+		double height = 165;
+		
+		// 身長が168㎝以上の場合のみ、身長を表示する
+		if (height >= 168) {
+			System.out.println(name + "の身長は" + height + "㎝です。");
+		}
+		System.out.println(name + "は" + age + "歳です。");
+
+	}
+
+}
