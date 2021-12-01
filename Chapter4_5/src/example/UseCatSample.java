f49428523e0242249c68e6e31c7212bc73c72b95
gensan-64 <genyuu6240@gmail.com>
2021-12-01 18:55:31 +0900

    メソッドの表示

--- Chapter4_5/src/example/UseCatSample.java
+++ Chapter4_5/src/example/UseCatSample.java
@@ -4,6 +4,8 @@ public class UseCatSample {
 
 	public static void main(String[] args) {
 		Cat tama = new Cat();
+		tama.name = "タマ";
+		
 		tama.eat();
 		tama.playToy("ひも");
 		boolean h = tama.isHungry();
