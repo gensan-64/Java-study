3ac50f334f06231b51304d635da60cf8958418af
gensan-64 <genyuu6240@gmail.com>
2021-12-13 18:28:31 +0900

    引数のあるコンストラクターの呼び出し

--- Chapter4_5/src/example/ConstructorSample.java
+++ Chapter4_5/src/example/ConstructorSample.java
@@ -1,22 +1,12 @@
 package example;
 
 public class ConstructorSample {
+    public static void main(String[] args) {
+        System.out.println("--- new Cat(\"タマ\")でインスタンスを生成する ---");
+        Cat tama = new Cat("タマ", 3);
 
-	public static void main(String[] args) {
-		System.out.println("--- new Cat()でインスタンスを生成する ---");
-		Cat tama = new Cat();
-		
-		System.out.println();
-		System.out.println("--- setName/setAgeする前に、自己紹介させる ---");
-		tama.introduceMyself();
-		
-		tama.setName("タマ");
-		tama.setAge(3);
-		
-		System.out.println();
-		System.out.println("--- setName/setAgeした後で、自己紹介させる ---");
-		tama.introduceMyself();
-
-	}
-
+        System.out.println();
+        System.out.println("--- インスタンス生成直後、自己紹介させる ---");
+        tama.introduceMyself();
+    }
 }
