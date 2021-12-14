ae1cfa3fe708e3b235b60851bb8b9c87edf7d57d
gensan-64 <genyuu6240@gmail.com>
2021-12-14 18:15:38 +0900

    thisの利用

--- Chapter4_5/src/example/ConstructorSample.java
+++ Chapter4_5/src/example/ConstructorSample.java
@@ -3,7 +3,7 @@ package example;
 public class ConstructorSample {
     public static void main(String[] args) {
         System.out.println("--- new Cat(\"タマ\")でインスタンスを生成する ---");
-        Cat tama = new Cat("タマ", 3);
+        Cat tama = new Cat("タマ");
 
         System.out.println();
         System.out.println("--- インスタンス生成直後、自己紹介させる ---");
